f79f4b41cad0c0e81b4456b776fc301051366653
Israel Torres <israel.a.torres01@gmail.com>
2020-03-20 23:35:22 -0400

    Assignment #4 completed

--- sync-async.js
+++ sync-async.js
@@ -1,16 +1,17 @@
 var http = require('http');
 function myname (){          
-  console.log("Here is my IP address");
+  return "Here is my IP address";
 }
 function callHttpbi() {
     let promise = new Promise((resolve, reject) => { 
-        http.get('http://httpbin.org/ip', function(response) {
+        http.get('http://httpbin.org/ip', 
+        async function(response) {
             var str="";
-            response.setEncoding('utf8');
-            response.on('data', function(data){
+            await response.setEncoding('utf8');
+            await response.on('data', function(data){
                 str += data;
             });
-            response.on('end', function() {
+            await response.on('end', function() {
                 var result = JSON.parse(str);
                 myips = result.origin;
                 resolve(myips)
@@ -18,10 +19,12 @@ function callHttpbi() {
         });
     });
 
-    let result = promise; 
+    return promise; 
 }
 async function executeAsyncTask(){ 
-  const valueA = await callHttpbin() 
+  const valueA = await callHttpbi() 
   const valueB = await myname(); 
   console.log(valueB+" "+valueA)
+}
+executeAsyncTask();
 // Output Here is my IP address 149.24.160.1, 149.24.160.1
